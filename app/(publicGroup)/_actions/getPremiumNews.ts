0a8f2e36e7dfc18ba096eae1fb65eef10d7806a9
Mohammad TanvirAhmmed <tanvir262728@gmail.com>
2026-08-18 00:36:21 +0600

    searching add

--- app/(publicGroup)/_actions/getPremiumNews.ts
+++ app/(publicGroup)/_actions/getPremiumNews.ts
@@ -2,7 +2,13 @@
 
 import { cookies } from "next/headers";
 
-export const getPremiumNews = async () => {
+export const getPremiumNews = async ({
+  query,
+}: {
+  query?: {
+    [key: string]: string | string[] | undefined;
+  };
+}) => {
   const cookiestore = await cookies();
 
   const accesstoken = cookiestore.get("accesstoken")?.value;
@@ -14,16 +20,40 @@ export const getPremiumNews = async () => {
     };
   }
 
-  const res = await fetch(`${process.env.BACKEND_URL}/api/premium`, {
-    headers: {
-      cookie: `accesstoken=${accesstoken}`,
+  // Convert searchParams object to query string
+  const searchParams = new URLSearchParams();
+
+  if (query) {
+    Object.entries(query).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        value.forEach((item) => {
+          searchParams.append(key, item);
+        });
+      } else if (value !== undefined) {
+        searchParams.set(key, value);
+      }
+    });
+  }
+
+  const queryString = searchParams.toString();
+
+  const res = await fetch(
+    `${process.env.BACKEND_URL}/api/premium${
+      queryString ? `?${queryString}` : ""
+    }`,
+    {
+      headers: {
+        cookie: `accesstoken=${accesstoken}`,
+      },
+      cache: "force-cache",
+      next: {
+        revalidate: 60 * 60 * 24,
+        tags: ["premium-news"],
+      },
     },
-    cache: "force-cache",
-    next: {
-      revalidate: 60 * 60 * 24,
-      tags: ["premium-news"],
-    },
-  });
+  );
+
   const result = await res.json();
+
   return result;
 };
